a53540501e22691190b4ef8b27b1aacd279ecb80
Shivam <shivamsaurav63@gmail.com>
2025-05-05 20:23:55 +0530

    checking for bugs

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,31 +1,21 @@
 'use client'
 import React from 'react'
-import Hero from './Hero/Hero'
-import About from './About/About'
-import Projects from './Projects/Projects'
-import Skills from './Skills/Skills'
-import Blogs from './Blogs/Blogs'
-import Contact from './Contact/Contact'
+// import Hero from './Hero/Hero'
+// import About from './About/About'
+// import Projects from './Projects/Projects'
+// import Skills from './Skills/Skills'
+// import Blogs from './Blogs/Blogs'
+// import Contact from './Contact/Contact'
 
 const Home = () => {
   return (
     <div>
-      <div><Hero/></div>
-      <div id="About">
-        <About/>
-        </div>
-      <div id='Blogs'>
-        <Blogs/>
-        </div>
-      <div id='Projects'>
-        <Projects/>
-        </div>
-      <div id='Skills'>
-        <Skills/>
-        </div>
-      <div id='Contact'>
-        <Contact/>
-        </div>
+      {/* <div><Hero/></div> */}
+      {/* <div id="About"><About/></div> */}
+      {/* <div id='Blogs'><Blogs/></div> */}
+      {/* <div id='Projects'><Projects/></div> */}
+      {/* <div id='Skills'><Skills/></div> */}
+      {/* <div id='Contact'><Contact/></div> */}
     </div>
   )
 }
